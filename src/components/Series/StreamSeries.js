52285b09e9513beb289edd9d05a1c179226843b5
certifiedTboy <etosin70@gmail.com>
2024-04-10 23:49:01 +0100

    add streaming

--- src/components/Series/StreamSeries.js
+++ src/components/Series/StreamSeries.js
@@ -1,4 +1,5 @@
 import { useParams } from "react-router-dom";
+import "./StreamSeries.css";
 
 const StreamSeries = () => {
   const params = useParams();
@@ -6,11 +7,27 @@ const StreamSeries = () => {
   const { seriesId } = params;
 
   return (
-    <iframe
-      width="100%"
-      height="700px"
-      src={`https://vidsrc.pro/embed/tv/${seriesId}/1/1`}
-    ></iframe>
+    <div className="stream-container">
+      <iframe
+        width="100%"
+        height="100vh"
+        src={`https://vidsrc.pro/embed/tv/ssss/1/1`}
+      ></iframe>
+
+      <div>
+        <div>
+          <select>
+            <option> Seasons </option>
+          </select>
+        </div>
+
+        <div>
+          <select>
+            <option> Episode </option>
+          </select>
+        </div>
+      </div>
+    </div>
   );
 };
 
